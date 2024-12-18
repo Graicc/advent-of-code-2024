0301b24e54305eb4337b5f019be44c3a422e8177
Graicc <33105645+Graicc@users.noreply.github.com>
2024-12-18 00:40:15 -0500

    Add day 18

--- day_18/aoc18.py
+++ day_18/aoc18.py
@@ -9,69 +9,66 @@ def add(a: Point, b: Point) -> Point:
     return Point(a.x + b.x, a.y + b.y)
 
 
-def times(a: Point, scaler: int) -> Point:
-    return Point(a.x * scaler, a.y * scaler)
-
-
 with open("day_18/input.txt", "r") as f:
-    data = [map(int, line.split(",")) for line in f.readlines()]
+    all_data = [map(int, line.split(",")) for line in f.readlines()]
+    all_data = [Point(x, y) for x, y in all_data]
 
 # size = 6
 size = 70
-# sim = 12
-sim = 1024
 
-data = set([Point(x, y) for x, y in data[:sim]])
 
-seen: Set[Point] = set()
-boundary = set()
-boundary.add(Point(0, 0))
+def distance(data) -> int:
+    seen: Set[Point] = set()
+    boundary = set()
+    boundary.add(Point(0, 0))
 
-end = Point(size, size)
+    end = Point(size, size)
 
-count = 1
-while len(boundary) > 0:
-    print(len(seen), len(boundary))
-    # print(boundry)
-    next_boundary = set()
-
-    for point in boundary:
-        seen.add(point)
-
+    count = 1
     found = False
-    for point in boundary:
-        for neighbor in [
-            add(point, Point(0, 1)),
-            add(point, Point(1, 0)),
-            add(point, Point(0, -1)),
-            add(point, Point(-1, 0)),
-        ]:
-            if (
-                neighbor.x < 0
-                or neighbor.y < 0
-                or neighbor.x > size
-                or neighbor.y > size
-            ):
-                continue
+    while len(boundary) > 0:
+        next_boundary = set()
 
-            if neighbor == end:
-                found = True
+        for point in boundary:
+            seen.add(point)
 
-            if neighbor not in data and neighbor not in seen:
-                next_boundary.add(neighbor)
+        for point in boundary:
+            for neighbor in [
+                add(point, Point(0, 1)),
+                add(point, Point(1, 0)),
+                add(point, Point(0, -1)),
+                add(point, Point(-1, 0)),
+            ]:
+                if (
+                    neighbor.x < 0
+                    or neighbor.y < 0
+                    or neighbor.x > size
+                    or neighbor.y > size
+                ):
+                    continue
 
-    if found:
-        break
+                if neighbor == end:
+                    return count
 
-    boundary = next_boundary
-    count += 1
+                if neighbor not in data and neighbor not in seen:
+                    next_boundary.add(neighbor)
 
-for y in range(size + 1):
-    for x in range(size + 1):
-        if Point(x, y) in seen:
-            print("O", end="")
-        if Point(x, y) in data:
-            print("#", end="")
-    print()
+        boundary = next_boundary
+        count += 1
 
-print(count)
+    return -1
+
+
+print(distance(set(all_data[:1024])))
+
+l = 0
+u = len(all_data)
+while u > l:
+    sim = (u + l) // 2
+    if distance(set(all_data[:sim])) != -1:
+        l = sim + 1
+    else:
+        u = sim - 1
+
+assert u == l
+print(f"{all_data[u].x},{all_data[u].y}")
