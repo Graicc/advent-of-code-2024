c7930a2ab844070bd066e45ff4dcf7e20d41f222
Graicc <33105645+Graicc@users.noreply.github.com>
2024-12-22 00:49:27 -0500

    Speed up day 22

--- day_22/aoc22.py
+++ day_22/aoc22.py
@@ -1,25 +1,20 @@
-import itertools
 from collections import defaultdict
 
 
-def mix(a, b):
-    return a ^ b
-
-
-def prune(a):
-    return a % 16777216
-
-
 def step(x: int) -> int:
-    p1 = prune(mix(x, x * 64))
-    p2 = prune(mix(p1, p1 // 32))
-    return prune(mix(p2, p2 * 2048))
+    x ^= x * 64
+    x %= 16777216
+    x ^= x // 32
+    x %= 16777216
+    x ^= x * 2048
+    x %= 16777216
+    return x
 
 
 with open("day_22/input.txt", "r") as f:
     data = map(int, f.read().strip().split("\n"))
 
-sum = 0
+p1 = 0
 
 diffs = defaultdict(list)
 vals = defaultdict(list)
@@ -32,30 +27,19 @@ for i, x in enumerate(data):
         vals[i].append(n % 10)
         x = n
 
-    sum += x
-print(sum)
+    p1 += x
+print(p1)
 
 
-import multiprocessing.pool
-
-sequnece_val_map = defaultdict(dict)
+score_map = defaultdict(int)
 for i, diff in diffs.items():
     val = vals[i]
+    seen = set()
     for j in range(len(diff) - 3):
         key = (diff[j], diff[j + 1], diff[j + 2], diff[j + 3])
-        if key not in sequnece_val_map[i]:
-            v = val[j + 3]
-            sequnece_val_map[i][key] = v
+        if key not in seen:
+            seen.add(key)
+            score_map[key] += val[j + 3]
 
 
-m = 0
-for a, b, c, d in itertools.product(
-    range(-9, 9), range(-9, 9), range(-9, 9), range(-9, 9)
-):
-    sum = 0
-    for i, x in sequnece_val_map.items():
-        sum += x.get((a, b, c, d), 0)
-    if sum > m:
-        m = sum
-
-print(m)
+print(max(score_map.values()))
